01168feda289b6ef851e1234deb5882ec745b22f
alessandro <alessandrodamata6@gmail.com>
2024-01-18 21:16:15 -0300

    fix: correção no provider de delete category by id

--- src/provider/Categories/DeleteCategoryProvider.js
+++ src/provider/Categories/DeleteCategoryProvider.js
@@ -1,16 +1,44 @@
 import { prisma } from "../../database/prismaClient.js";
+import { deletePhoto } from "../../services/deletePhoto/index.js";
 
 export const deleteCategoryProvider = async (categoryId) => {
 	try {
+
+		await prisma.ratings.deleteMany({
+			where: {promotion: {categoryId: categoryId}}
+		});
+
+		await prisma.comments.deleteMany({
+			where: {promotion: {categoryId: categoryId}}
+		});
+
+		const promotionsWithPhotos = await prisma.promotions.findMany({
+			where: {categoryId: categoryId},
+			select: {
+				photo: true,
+			}
+		});
+
+		await prisma.promotions.deleteMany({
+			where: {categoryId: categoryId}
+		});
+
 		const category = await prisma.categories.delete({
 			where: { categoryId: categoryId}
 		});
 
-		if(!category){
+		if(category){
+			if(promotionsWithPhotos && promotionsWithPhotos.length > 0){
+				await Promise.all(promotionsWithPhotos.map(async photo => 
+					await deletePhoto(`/uploads/promotion/${photo.photo}`)
+				));
+			}
+			return category.categoryId;
+			
+		} else {
 			return new Error("error ao deletar categoria.");
 		}
 
-		return category.categoryId;
 	} catch (error) {
 		// console.log(`ERROR DELETE CATEGORY: ${error}`);
 		return new Error("error ao deletar categoria.");
